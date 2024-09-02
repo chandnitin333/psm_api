b18ef96ac353986e78177919af3fb4c5fc6a9b84
nitinchandekar <chand.nitin333@gmail.com>
2024-09-02 22:41:06 +0530

    changes

--- src/controller/TalukaController.ts
+++ src/controller/TalukaController.ts
@@ -5,17 +5,18 @@ export class TalukaController {
 
     static async createTaluka(req, res, next) {
         try {
+           
             let { name, district_id } = req.body;
             let params = {
-                id: district_id,
+                id: parseInt(district_id),
                 searchText: name
             }
             let availableTaluka = await findTaluka(params);
-            if(availableTaluka.length > 0){
-                let response = ApiResponse.successResponse(res, name+ " Taluka already exists");
+            if (availableTaluka.length > 0) {
+                let response = ApiResponse.successResponse(res, name + " Taluka already exists");
                 return response;
             }
-            let taluka: any = await createTaluka(name, district_id);
+            let taluka: any = await createTaluka(name, parseInt(district_id));
             let response = ApiResponse.successResponse(res, taluka.name + " Taluka created successfully");
             return response;
         } catch (error) {
@@ -26,7 +27,7 @@ export class TalukaController {
 
     static async getAllTaluka(req, res, next) {
         try {
-             let offset:number = parseInt(process.env.PAGE_OFFSET) || 0;
+            let offset: number = parseInt(process.env.PAGE_OFFSET) || 0;
             let pageNumber = (req.body?.pageNumber ?? 1) - 1;
             let searchText = req.body?.searchText ?? "";
             let limit = (pageNumber != 0) ? pageNumber * offset : pageNumber;
@@ -49,7 +50,7 @@ export class TalukaController {
         try {
             let data = req.body;
             let taluka = await getTaluka(parseInt(data.id));
-            if(taluka == null){
+            if (taluka == null) {
                 let response = ApiResponse.successResponseWithData(res, "Taluka not found", []);
                 return response;
             }
@@ -64,7 +65,7 @@ export class TalukaController {
     static async updateTaluka(req, res, next) {
         try {
             let data = req.body;
-            let taluka = await updateTaluka(parseInt(data.id), { name: data.name, district_id: data.district_id });
+            let taluka = await updateTaluka(parseInt(data.id), { name: data.name, district_id: parseInt(data.district_id) });
             let response = ApiResponse.successResponseWithData(res, "Taluka updated successfully", taluka);
             return response;
         } catch (error) {
@@ -75,13 +76,13 @@ export class TalukaController {
 
     static async deleteTaluka(req, res, next) {
         try {
-            let data = req.body;
-            let existTaluka = await getTaluka(parseInt(data.id));
-            if(existTaluka == null){
+            let id = req.params.id;
+            let existTaluka = await getTaluka(parseInt(id));
+            if (existTaluka == null) {
                 let response = ApiResponse.successResponse(res, "Sorry we'r unable to find this taluka for delete operation");
                 return response;
             }
-            let taluka = await deleteTaluka(parseInt(data.id));
+            let taluka = await deleteTaluka(parseInt(id));
             let response = ApiResponse.successResponseWithData(res, "Taluka deleted successfully", taluka);
             return response;
         } catch (error) {
